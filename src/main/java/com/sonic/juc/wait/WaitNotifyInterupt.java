f80ebdd06bbc39c9f0f0fb1e88c0e8bbb6dafd10
AK47Sonic <675737965@qq.com>
2019-01-06 13:06:44 +0800

    add wait

--- src/main/java/com/sonic/juc/wait/WaitNotifyInterupt.java
+++ src/main/java/com/sonic/juc/wait/WaitNotifyInterupt.java
@@ -17,7 +17,7 @@ public class WaitNotifyInterupt {
                 System.out.println("---begin---");
                 try {
                     synchronized (obj) {
-                        obj.wait(); //没有考虑虚假唤醒
+                        obj.wait(0); //没有考虑虚假唤醒
                     }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
