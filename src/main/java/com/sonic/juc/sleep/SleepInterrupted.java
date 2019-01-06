798ad3d88d82f849728655beef2acc8cceff9292
AK47Sonic <675737965@qq.com>
2019-01-07 07:51:51 +0800

    add sleep

---
+++ src/main/java/com/sonic/juc/sleep/SleepInterrupted.java
@@ -0,0 +1,29 @@
+package com.sonic.juc.sleep;
+
+/**
+ * SleepInterrupted
+ *
+ * @auther Sonic
+ * @since 2019/1/7
+ */
+public class SleepInterrupted {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            try {
+                System.out.println("child thread is in sleep");
+                Thread.sleep(5000);
+                System.out.println("child thread is in awaked");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("child thread is over");
+        });
+
+        thread.start();
+
+        Thread.sleep(2000);
+
+        thread.interrupt();
+
+    }
+}
---
+++ src/main/java/com/sonic/juc/sleep/SleepLock.java
@@ -0,0 +1,50 @@
+package com.sonic.juc.sleep;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * SleepLock
+ * lock 如果不unlock, 即使线程A已经结束，其他线程仍然无法访问
+ *
+ * @auther Sonic
+ * @since 2019/1/7
+ */
+public class SleepLock {
+
+    private static final Lock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        Thread threadA = new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println("child threadA is in sleep");
+                Thread.sleep(5000);
+                System.out.println("child threadA is in awaked");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+            System.out.println("child threadA is over");
+        });
+
+        Thread threadB = new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println("child threadB is in sleep");
+                Thread.sleep(5000);
+                System.out.println("child threadB is in awaked");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+            System.out.println("child threadB is over");
+        });
+
+        threadA.start();
+        threadB.start();
+
+    }
+}
