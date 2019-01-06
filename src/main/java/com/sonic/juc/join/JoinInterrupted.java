f7c4b58a327c077808f2da43d2d17e747b059c4e
AK47Sonic <675737965@qq.com>
2019-01-07 07:15:25 +0800

    add join

---
+++ src/main/java/com/sonic/juc/join/JoinInterrupted.java
@@ -0,0 +1,47 @@
+package com.sonic.juc.join;
+
+/**
+ * JoinInterrupted
+ *
+ * @auther Sonic
+ * @since 2019/1/7
+ */
+public class JoinInterrupted {
+
+    public static void main(String[] args) {
+        Thread threadOne = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("threadOne begin run!");
+                for (; ; ) {
+
+                }
+            }
+        });
+
+        final Thread mainThread = Thread.currentThread();
+
+        Thread threadTwo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                mainThread.interrupt();
+            }
+        });
+
+        threadOne.setDaemon(true);
+        threadOne.start();
+        threadTwo.start();
+
+        try {
+            threadOne.join();
+        } catch (InterruptedException e) {
+            System.out.println("main thread: " + e);
+        }
+
+    }
+}
