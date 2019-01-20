95703e06518c75f056d64d2b00599c36b7a86936
AK47Sonic <675737965@qq.com>
2019-01-20 08:50:10 +0800

    add LongAdder, LongAccumulator

--- src/main/java/com/sonic/juc/atomic/AtomicTest.java
+++ src/main/java/com/sonic/juc/atomic/AtomicTest.java
@@ -1,6 +1,6 @@
 package com.sonic.juc.atomic;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAccumulator;
 
 /**
  * AtomicTest
@@ -10,7 +10,9 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class AtomicTest {
 
-    private static AtomicLong atomicLong = new AtomicLong();
+    //    private static AtomicLong atomicLong = new AtomicLong();
+//    private static LongAdder longAdder = new LongAdder();
+    private static LongAccumulator longAccumulator = new LongAccumulator((x, y) -> x + y, 0);
 
     private static Integer[] arrayOne = new Integer[]{0, 1, 2, 3, 0, 5, 6, 0, 7};
     private static Integer[] arrayTwo = new Integer[]{10, 1, 2, 3, 0, 5, 6, 0, 11};
@@ -20,7 +22,9 @@ public class AtomicTest {
             int size = arrayOne.length;
             for (int i = 0; i < size; i++) {
                 if (arrayOne[i].intValue() == 0) {
-                    atomicLong.incrementAndGet();
+//                    atomicLong.incrementAndGet();
+//                    longAdder.increment();
+                    longAccumulator.accumulate(1L);
                 }
             }
         });
@@ -29,7 +33,9 @@ public class AtomicTest {
             int size = arrayTwo.length;
             for (int i = 0; i < size; i++) {
                 if (arrayTwo[i].intValue() == 0) {
-                    atomicLong.incrementAndGet();
+//                    atomicLong.incrementAndGet();
+//                    longAdder.increment();
+                    longAccumulator.accumulate(1L);
                 }
             }
         });
@@ -40,7 +46,9 @@ public class AtomicTest {
         threadOne.join();
         threadTwo.join();
 
-        System.out.println("count 0: " + atomicLong.get());
+//        System.out.println("count 0: " + atomicLong.get());
+//        System.out.println("count 0: " + longAdder.sum());
+        System.out.println("count 0: " + longAccumulator.get());
 
     }
 
---
+++ src/main/java/com/sonic/juc/atomic/LongAdderTest.java
@@ -0,0 +1,37 @@
+package com.sonic.juc.atomic;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * LongAdderTest
+ *
+ * @auther Sonic
+ * @since 2019/1/19
+ */
+public class LongAdderTest {
+
+    static LongAdder longAdder = new LongAdder();
+    static final long MAX_VALUE =1000;
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        Runnable task = () -> {
+            long sum = longAdder.sum();
+            while (sum < MAX_VALUE) {
+                longAdder.increment();
+                sum = longAdder.sum();
+            }
+            System.out.println("longAdder sum: " + sum);
+        };
+
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(task);
+        }
+
+    }
+
+}
