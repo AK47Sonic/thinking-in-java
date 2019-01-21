df9e819a652343a8dffdb010b7faf06a801bf4f3
AK47Sonic <675737965@qq.com>
2019-01-21 21:14:26 +0800

    add Semaphore

---
+++ src/main/java/com/sonic/juc/semaphore/SemaphoreTest.java
@@ -0,0 +1,46 @@
+package com.sonic.juc.semaphore;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * SemaphoreTest
+ *
+ * @auther Sonic
+ * @since 2019/1/21
+ */
+public class SemaphoreTest {
+
+    static Semaphore semaphore = new Semaphore(1);
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread threadOne = new Thread(() -> {
+            try {
+                semaphore.acquire();
+                System.out.println("Before Thread name: " + Thread.currentThread().getName() + ", threadOne");
+                Thread.sleep(10000);
+                System.out.println("After Thread name: " + Thread.currentThread().getName() + ", threadOne");
+                semaphore.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread threadTwo = new Thread(() -> {
+            try {
+                semaphore.acquire();
+                System.out.println("Thread name: " + Thread.currentThread().getName() + ", threadTwo");
+                semaphore.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        threadOne.start();
+        Thread.sleep(1000);
+        threadTwo.start();
+
+        threadOne.join();
+        threadTwo.join();
+    }
+
+}
