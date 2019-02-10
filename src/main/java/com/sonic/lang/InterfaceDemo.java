3eb8064d8f59bae48454f9c464de5d83198b127d
AK47Sonic <675737965@qq.com>
2019-02-10 14:45:15 +0800

    Java learning

---
+++ src/main/java/com/sonic/lang/InterfaceDemo.java
@@ -0,0 +1,12 @@
+package com.sonic.lang;
+
+/**
+ * InterfaceDemo
+ *
+ * @author Sonic
+ * @since 2019/2/10
+ */
+public interface InterfaceDemo {
+    public static final int VALUE = 1; //public static final没有必要写
+
+}
