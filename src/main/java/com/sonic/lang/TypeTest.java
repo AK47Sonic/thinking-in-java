5a705bd1b38462fb64a0a86a9959c611a12fd4bf
AK47Sonic <675737965@qq.com>
2019-02-10 22:21:06 +0800

    Java learning

---
+++ src/main/java/com/sonic/lang/TypeTest.java
@@ -0,0 +1,16 @@
+package com.sonic.lang;
+
+import java.lang.reflect.Type;
+
+/**
+ * TypeTest
+ *
+ * @author Sonic
+ * @since 2019/2/10
+ */
+public class TypeTest {
+    public static void main(String[] args) {
+        Type type = int.class; //1.5以后
+        Class typeClass = int.class; //所有版本
+    }
+}
