6eed1222150e1f7cf09133eb30e4a5c69badd82a
AK47Sonic <675737965@qq.com>
2019-07-18 06:24:01 +0800

    Java

---
+++ src/main/java/com/sonic/qapd/AnimalFarm.java
@@ -0,0 +1,19 @@
+package com.sonic.qapd;
+
+/**
+ * AnimalFarm
+ *
+ * "length: 10" == "Animals are equal: length: " 是false
+ *
+ * @author Sonic
+ * @since 2019/7/18
+ */
+public class AnimalFarm {
+
+    public static void main(String[] args) {
+        final String pig = "length: 10";
+        final String dog = "length: " + pig.length();
+        System.out.println("Animals are equal: "
+                + pig == dog);
+    }
+}
