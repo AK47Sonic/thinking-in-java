826ca1673efbd674accb5dca239f94ca2dbfcfd7
AK47Sonic <675737965@qq.com>
2020-01-20 21:59:29 +0800

    2020/1/20

--- src/main/java/com/sonic/difficulty/Elvis.java
+++ src/main/java/com/sonic/difficulty/Elvis.java
@@ -8,12 +8,19 @@ package com.sonic.difficulty;
  */
 public class Elvis {
 
-    public static final Elvis ELVIS = new Elvis();
+    public static Elvis ELVIS = new Elvis();
+    private static final Boolean LIVING;
 
-    private Elvis() {
+    static {
+        System.out.println("init");
+        LIVING = true;
     }
 
-    private static final Boolean LIVING = true;
+    private Elvis() {
+        System.out.println(":" + LIVING);
+    }
+
+    public static Elvis ELVIS2 = new Elvis();
 
     private final Boolean alive = LIVING;
 
@@ -22,6 +29,8 @@ public class Elvis {
     }
 
     public static void main(String[] args) {
+        System.out.println("ELVIS:" + ELVIS.lives());
+        System.out.println("ELVIS2:" + ELVIS2.lives());
         System.out.println(ELVIS.lives() == null);
 //        System.out.println(ELVIS.lives() ? "Hound Dog" : "Heartbreak Hotel");
     }
